87247edb213d944f9efd1652a372260ee74b9ccf
ppiszczekcode <p.piszczek.stuff+github@gmail.com>
2025-05-12 22:30:18 +0200

    'starting-working-on-downloader'

--- src/downloader.py
+++ src/downloader.py
@@ -1,42 +1,13 @@
-import subprocess, os, json
-
-#returns spotdl_wrap path
-from utils import get_main_path
-#returns folder path
-from utils import create_playlist_folder
-
 # # Zadania:
 # # Składa komendę spotdl na podstawie settings
 # # Obsługuje błędy
 # # Zwraca sukces / fail
 
-def 
-# wywołanie spotdl z opcjami z settings
-def download(get_input(), folder_name, settings):
-    main_path = get_main_path()
-    folder_path = create_playlist_folder()
+import subprocess, os, json
+from utils import get_main_path
 
-    if not os.path.exists(folder_path):
-        os.makedirs(folder_path)
 
-    ensure_logs_directory()  # Upewnij się, że folder logs istnieje
-
-#     command = [
-#         "spotdl", "download", url,
-#         "--skip-album-art",
-#         "--print-errors",
-#         "--save-errors", os.path.join(main_path, "spotdl", "logs", "errors.txt"),
-#         "--overwrite", "skip",
-#         "--threads", "8",
-#         "--bitrate", settings["bitrate"],
-#         "--output", os.path.join(folder_path, "{artist} - {title}.mp3"),
-#         "--format", settings["format"]
-#     ]
-
-#     try:
-#         subprocess.run(command, check=True)
-#         return True
-#     except subprocess.CalledProcessError as e:
-#         print(f"Nie udało się pobrać: {url}, błąd: {e}")
-#         return False
-
+if __name__ == "__main__":
+    print("test")
+    print(get_main_path())
+    
